6eb4962fe31adc5fbb378e86936b39daa24da971
Gmafuo <matheogirard@hotmail.com>
2025-02-23 13:32:48 +0100

    feat: Ajout de fond animé + corrections de bug

--- dreambeats/src/config/scenes.js
+++ dreambeats/src/config/scenes.js
@@ -2,6 +2,11 @@ import moonGirlVideo from '../assets/scenes/moon-girl/video.mp4'
 import moonGirlImage from '../assets/scenes/moon-girl/thumbnail.webp'
 import trainGirlVideo from '../assets/scenes/train-girl/video.mp4'
 import trainGirlImage from '../assets/scenes/train-girl/thumbnail.webp'
+import natureWorkVideo from '../assets/scenes/nature-work/video.mp4'
+import natureWorkImage from '../assets/scenes/nature-work/thumbnail.png'
+import montaignWorkVideo from '../assets/scenes/montaign-work/video.mp4'
+import montaignWorkImage from '../assets/scenes/montaign-work/thumbnail.png'
+
 
 export const SCENES = [
   {
@@ -19,10 +24,17 @@ export const SCENES = [
     theme: 'cool'
   },
   {
-    id: 'train-girl',
-    title: 'Train Girl',
-    video: trainGirlVideo,
-    image: trainGirlImage,
-    theme: 'cool'
+    id: 'nature-work',
+    title: 'Nature Work',
+    video: natureWorkVideo,
+    image: natureWorkImage,
+    theme: 'nature'
+  },
+  {
+    id: 'montaign-work',
+    title: 'Montaign Work',
+    video: montaignWorkVideo,
+    image: montaignWorkImage,
+    theme: 'nature'
   }
 ]; 
